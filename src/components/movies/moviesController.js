7a47ffb475cd8f3e993669d60c12799cf281f0a0
José <jose@eficilog.com>
2019-11-22 16:05:14 -0300

    added movies controller

---
+++ src/components/movies/moviesController.js
@@ -0,0 +1,22 @@
+import { Router } from 'express'
+import database from '../../database'
+import { getProducerMinMaxWinInterval } from './moviesDAL'
+
+const router = Router()
+
+router.get('/', (req, res) => {
+  const resource = req.query.resource
+  if (resource) {
+    if (resource === 'producersMinMaxWinInterval') {
+      const data = getProducerMinMaxWinInterval()
+      return res.json({ data })
+    }
+    return res.status(404).json({
+      error: `Resource '${resource}' was not found.`
+    })
+  }
+
+  return res.json({ data: database })
+})
+
+export default router
